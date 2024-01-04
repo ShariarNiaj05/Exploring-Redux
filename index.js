6ab641280a7886b707c1c5741ae6d8f8149e0398
Shariar Islam <shariarn85@gmail.com>
2024-01-04 15:57:58 +0600

    redux | Reducers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const INCREMENT = 'INCREMENT'
 const DECREMENT = 'DECREMENT'
-const ADD_USER = 'ADD_USER'
+// const ADD_USER = 'ADD_USER'
 
 // state
 const initialCounterState = {
@@ -31,10 +31,31 @@ const decrementCounter = () => {
     }
 }
 
+// create reducer for counter 
+const counterReducer = (state = initialCounterState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
 
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+
+
+        default:
+            state
+    }
+}
+
+/* 
 const addUser = () => {
     return {
         type: ADD_USER,
         payload: { name: 'Niaj' }
     }
-}
+} */
