c9ef213e13ec666405d656ffe0611d1bef2d10dc
Shariar Islam <shariarn85@gmail.com>
2024-01-07 00:32:03 +0600

    Counter app using redux toolkit

---
+++ Counter app using redux toolkit/Counter-app-using-redux-toolkit/src/app/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import  counterReducer  from "../Features/Counter/counterSlice";
+
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer
+    }
+})
+
+
+export default store;
