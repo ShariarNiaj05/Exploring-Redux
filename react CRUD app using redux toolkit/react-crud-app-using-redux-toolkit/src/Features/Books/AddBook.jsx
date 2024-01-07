1a3a213944e3e56d36e27b130f3e676360fb2a4d
Shariar Islam <shariarn85@gmail.com>
2024-01-07 22:51:04 +0600

    create and read operation completed

--- react CRUD app using redux toolkit/react-crud-app-using-redux-toolkit/src/Features/Books/AddBook.jsx
+++ react CRUD app using redux toolkit/react-crud-app-using-redux-toolkit/src/Features/Books/AddBook.jsx
@@ -1,15 +1,56 @@
- 
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addBook } from "./BookSlice";
+import { useNavigate } from "react-router-dom";
+
 const AddBook = () => {
- return (
-    <div> 
-         I am available from AddBook 
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const numberOfBooks = useSelector((state) => state.booksReducer.books.length);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = numberOfBooks + 1;
+    const book = { id, title, author };
+    dispatch(addBook(book));
+    navigate("/show-books", { replace: true });
+  };
+
+  return (
+    <div>
+      <h2>Add Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label htmlFor="title">Title: </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-field">
+          <label htmlFor="author">Author: </label>
+          <input
+            type="text"
+            id="author"
+            name="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Add Book</button>
+      </form>
     </div>
- )
-}
- 
-export default AddBook
- 
- 
-AddBook.propTypes = {
- 
+  );
 };
+
+export default AddBook;
+
+AddBook.propTypes = {};
--- react CRUD app using redux toolkit/react-crud-app-using-redux-toolkit/src/Features/Books/BooksView.jsx
+++ react CRUD app using redux toolkit/react-crud-app-using-redux-toolkit/src/Features/Books/BooksView.jsx
@@ -1,5 +1,40 @@
+import { useSelector } from "react-redux";
+
 const BooksView = () => {
-  return <div>I am available from BooksView</div>;
+  const books = useSelector((state) => state.booksReducer.books);
+
+  return (
+    <div>
+      <h2>List of Books</h2>
+      <table>
+        <thead>
+          <tr>
+            {/* <th>ID</th> */}
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books &&
+            books.map((book) => {
+              const { id, title, author } = book;
+              return (
+                <tr key={id}>
+                  {/* <td>{id}</td> */}
+                  <td>{title}</td>
+                  <td>{author}</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default BooksView;
